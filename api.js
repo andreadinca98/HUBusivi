81fd73cd8599615f1b2ce40911dd5a61008f3fc9
Andrea D'Incà <andrea.dinca-1@studenti.unitn.it>
2018-11-08 17:55:28 +0100

    Prima app

---
+++ api.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(PORT, () => console.log('Wxample app listening on port' + PORT))
