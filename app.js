1b2695e6b18607f3f04be2e9a07aa9b13039a424
Stefano <stefano.pretto@unitn.it>
2018-11-27 14:01:55 +0100

    vari percorsi e nodemon

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
+
+
+
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get("/")
+
+
 app.listen(PORT, () => console.log('Wxample app listening on port' + PORT))
