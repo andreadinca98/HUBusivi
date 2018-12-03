5713e77581b9b291a14455170c1e029c1f225f16
ilariaenache <ilaria.enache@studenti.unitn.it>
2018-12-03 14:11:44 +0100

    commit

--- routes/exam.js
+++ routes/exam.js
@@ -1,12 +1,19 @@
 //Ilaria
 const express = require('express')
-const router = express.Router();
+const examRouter = express.Router();
 
 //
 router.get('/exam', (req,res) =>{
     res.send("prova exam")
 })
 
+//parametri: name, examId
+
+//POST adds a exam on the database
+
+//DELETE removes an exam
+
+//PUT updates an exam
 
 
-module.exports = router
+module.exports = examRouter
