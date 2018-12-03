bb0d98fdb92f97a01f643ca92201cda816abcb70
ilariaenache <ilaria.enache@studenti.unitn.it>
2018-12-03 14:28:01 +0100

    exam

--- routes/exam.js
+++ routes/exam.js
@@ -1,12 +1,21 @@
 //Ilaria
 const express = require('express')
+const Exam = require('../models/exam')
 const examRouter = express.Router();
 
 //
-router.get('/exam', (req,res) =>{
+examRouter.get('/exam', (req,res) =>{
     res.send("prova exam")
 })
 
+examRouter.post('/exam', async function (req, res) {
+	var exam = new Exam();
+	exam.name = req.body.name;
+
+	// save the bear and check for errors
+	saved = await exam.save()
+	res.json(saved)
+})
 //parametri: name, examId
 
 //POST adds a exam on the database
