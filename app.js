db6dd11fa15583fab1cd472b1c395cd990bee33e
Giulio <giulio.mattolin@studenti.unitn.it>
2018-12-04 13:55:18 +0100

    push per presentazione

--- app.js
+++ app.js
@@ -33,14 +33,7 @@ app.listen(PORT, () => console.log('Example app listening on port: ' + PORT))
 //vari routers alle varie pagine 
 app.use('/teachers',routerTeacher)
 app.use(routerCourses)
-<<<<<<< HEAD
-app.use(routerMarks)
-app.use(routerExam)
-//app.use(routerAssignment)
-=======
 app.use('/marks',routerMarks)
-
->>>>>>> 621da18d7df8e46ac77637f75c1252318a2669d7
 app.use('/students',routerStudent);
 app.use(routerAssignment)
 
--- models/course.js
+++ models/course.js
@@ -3,10 +3,7 @@ const mongoose = require('mongoose');
 const courseSchema = mongoose.Schema({
     name: String,
     description : String,
-<<<<<<< HEAD
     t_id: String,
-=======
->>>>>>> 621da18d7df8e46ac77637f75c1252318a2669d7
     s_id: String
 })
 
