50d89946acc15caa90bd306dd5b290eeb9830141
Andrea D'Incà <andrea.dinca-1@studenti.unitn.it>
2018-12-16 18:46:34 +0100

    Aggiunto funzionalità

--- models/assignment.js
+++ models/assignment.js
@@ -5,7 +5,7 @@ const assignmentSchema = mongoose.Schema({
 	_id: mongoose.Schema.Types.ObjectId,
 	text: { type:String, require: true },
 	name: { type:String, require: true },
-	expireData: { type:String, require: true },
+	expireData: { type:String},
 	uploadData: { type:String, require: true },
 	studentId: { type: String, require: true },
 	courseId: { type: String, require: true },
