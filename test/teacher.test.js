9048bfc03174c873d391873b193c335ad44e68bc
Andrea D'Incà <andrea.dinca-1@studenti.unitn.it>
2018-12-18 17:29:40 +0100

    Risolto problemi travis

--- test/teacher.test.js
+++ test/teacher.test.js
@@ -3,67 +3,7 @@ const jwt     = require('jsonwebtoken'); // used to create, sign, and verify tok
 const config  = require('../config.js'); // get our config file
 const app     = require('../app');
 
-test('app module should be defined', () => {
-  expect(app).toBeDefined();
-});
-
-/*describe('Protected API /api/v2/teachers', () => {
-
-  // Moking User.findOne method
-  let userSpy;
-
-  beforeAll(() => {
-    const User = require('');
-    userSpy = jest.spyOn(User, 'findOne').mockImplementation((criterias) => {
-      return {
-        id: 1212,
-        name: 'John'
-      };
-    });
-  });
-
-  afterAll(() => {
-    userSpy.mockRestore();
-  });
-  */
-  test('GET /api/v2/teachers/:teacherId should return 200', async () => {
-    const response = await request(app).get('/api/v2/teachers');
-    expect(response.statusCode).toBe(200);
-  });
-/*
-  test('GET /api/v1/users/me?token=<invalid> should return 403', async () => {
-    const response = await request(app).get('/api/v1/users/me?token=123456');
-    expect(response.statusCode).toBe(403);
-  });
-
-  // create a valid token
-  var payload = {
-    id: 1212,
-    name: 'John',
-    admin: true
-  }
-  var options = {
-    expiresIn: 86400 // expires in 24 hours
-  }
-  var token = jwt.sign(payload, config.superSecret, options);
-      
-  test('GET /api/v1/users/me?token=<valid> should return 200', async () => {
-    const response = await request(app).get('/api/v1/users/me?token='+token);
-    expect(response.statusCode).toBe(200);
-  });
-
-  test('GET /api/v1/users/me?token=<valid> should return user information', async () => {
-    const response = await request(app).get('/api/v1/users/me?token='+token);
-    const user = response.body;
-    expect(user).toBeDefined();
-    expect(user.id).toBe(1212);
-    expect(user.name).toBe('John');
-  });
-});
-
-
-
-/*var request = require('supertest')
+var request = require('supertest')
 var teacher = require('../routes/students');
 var jwt = require('jsonwebtoken')
 const config  = require('../config.js'); // get our config file
@@ -119,4 +59,4 @@ describe('DELETE Teacher', function(){
         process.exit(0)
     })
 
-})*/
+})
